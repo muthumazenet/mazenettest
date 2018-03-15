00b394b0673d5cdbb060e0beacef70505482ab81
admin <admin@gmail.com>
2018-03-15 11:28:47 +0530

    calculate Length  New jgg

--- CalculatorTest.php
+++ CalculatorTest.php
@@ -1,10 +1,13 @@
 <?php
-// CalculatorTest.php
-include_once("Calculator.php");
 class CalculatorTest extends \PHPUnit_Framework_TestCase {
-    public function testDivideByPositiveNumber() {
-        $calcMock = new Calculator();
-        $this->assertFalse($calcMock->divideBy('123456789'));
+	 const MIN_LENGTH = 6;
+const MAX_LENGTH = 20;
+    public function testDivideByPositiveNumber() {		
+        $this->assertFalse($this->divideBy('123456789'));
     }
-   
+  
+    public function divideBy($num2) {
+        $length=strlen($num2);
+        return $length >= self::MIN_LENGTH && $length <= self::MAX_LENGTH;
+    }   
 }
